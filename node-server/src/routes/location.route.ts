7507d883583cdee3d5071eb4ab0210b042935d14
Ashish Mittal <aashishrbmittal@gmail.com>
2026-03-24 18:22:42 +0530

    Updated Location Controller & Routes.

--- node-server/src/routes/location.route.ts
+++ node-server/src/routes/location.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { liveLocation } from "../controllers/location.comtroller";
+import { liveLocation } from "../controllers/location.controller";
 
 const router = express.Router();
 
