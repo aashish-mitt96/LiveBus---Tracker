cacb2e0a10e23ee85de9a6eedd6243e1a65f18a3
Ashish Mittal <aashishrbmittal@gmail.com>
2026-07-03 10:42:30 +0530

    Added Get Stops Route.

--- node-server/src/routes/trip.route.ts
+++ node-server/src/routes/trip.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { startTrip, endTrip } from '../controllers/trip.controller';
+import { startTrip, endTrip, getStops } from '../controllers/trip.controller';
 import { pinStop } from '../controllers/stops.controller';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post ('/start-trip',       startTrip);
 router.patch("/end-trip/:tripId", endTrip);
 router.post ("/:tripId/pin-stop", pinStop);
+router.get("/:tripId/stops", getStops);
 
 export default router;
